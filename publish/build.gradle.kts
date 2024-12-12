885bc5f33540b3ede454b8ed7713d7c8a510c025
gyb1314 <gyb_1314@126.com>
2024-12-12 14:43:31 +0800

    kit release 0.2.0

--- publish/build.gradle.kts
+++ publish/build.gradle.kts
@@ -59,7 +59,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.ti-net-project"
             artifactId = "VSClientKit-Android"
-            version = "0.1.0"
+            version = "0.2.0"
 
             afterEvaluate {
                 from(components["release"])
